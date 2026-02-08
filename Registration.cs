8136a3a39596344eb8ad91725517d771c34f931c
RottenOtter14111 <arogers@anamosa.k12.ia.us>
2026-02-08 01:32:01 +0000

    done??

--- Registration.cs
+++ Registration.cs
@@ -1,5 +1,16 @@
-﻿namespace knightmoves;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace knightmoves;
 public class Registration
 {
     // Add your code here
+    public bool OldEnough = false;
+    public string ValidateAge()
+    {
+        if (OldEnough)
+        {
+            return "Old enough";
+        }
+        return "";
+    }
 }
